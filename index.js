49ad8d7249b98b2c2364c9b9bac52e4141d497ec
MikhailV89 <114851579+MikhailV89@users.noreply.github.com>
2023-04-29 09:55:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+value = prompt('Write few numbers?');
+newValue = value.toString().replaceAll('', ' ');
+console.log(newValue);
+
+str1 = prompt('Write first string?');
+str2 = prompt('Write second string?');
+str3 = prompt('Write third string?');
+result = str2 + str1 + str3;
+alert(result);
